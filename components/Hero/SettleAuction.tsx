34378d796fc5fc31850194a2345bec3efb7e263d
Volky <volkyeth@proton.me>
2023-01-22 14:43:54 -0300

    Fix Settle button text color

--- components/Hero/SettleAuction.tsx
+++ components/Hero/SettleAuction.tsx
@@ -5,6 +5,7 @@ import {
   useWaitForTransaction,
 } from "wagmi";
 import { AuctionABI } from "@buildersdk/sdk";
+import {useTheme} from "@/hooks/useTheme";
 
 export const SettleAuction = ({ auction }: { auction?: string }) => {
   const { config } = usePrepareContractWrite({
@@ -19,11 +20,12 @@ export const SettleAuction = ({ auction }: { auction?: string }) => {
   });
 
   const isLoading = contractLoading || transactionLoading;
+  const [theme] = useTheme();
 
   return (
     <button
       onClick={() => write?.()}
-      className="w-full bg-skin-button-accent hover:bg-skin-button-accent-hover h-12 text-white mt-6 rounded-lg flex items-center justify-around"
+      className="w-full bg-skin-button-accent bg-skin-button-accent hover:bg-skin-button-accent-hover text-skin-inverted h-12 mt-6 rounded-lg flex items-center justify-around"
     >
       {isLoading ? (
         <Image src="/spinner.svg" height={26} width={26} alt="spinner" />
